f0eb9820ae9b60598a211d9a39369e93228c563d
Shchupakov Antony <a.shchupakov@mail.ru>
2017-12-16 16:13:19 +0500

    fixed

--- src/calculator/Operand.java
+++ src/calculator/Operand.java
@@ -1,5 +1,6 @@
 package calculator;
 
+import complex.ComplexNumber;
 import token.Token;
 
 import java.util.HashMap;
@@ -17,8 +18,8 @@ public class Operand implements IOperable {
             operand.put(token.getType(), token.getValue());
         else if (token.getType().equals("complex")){
             ComplexNumber complex = (ComplexNumber) token.getValue();
-            if (complex.getRealPart() != 0.0) operand.put("real", complex.getRealPart());
-            if (complex.getImaginaryPart() != 0.0) operand.put("imaginary", complex.getImaginaryPart());
+            if (complex.real() != 0.0) operand.put("real", complex.real());
+            if (complex.imaginary() != 0.0) operand.put("imaginary", complex.imaginary());
         }
     }
 }
