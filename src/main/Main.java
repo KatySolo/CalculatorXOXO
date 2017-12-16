445b8a356436d4d6c1e67be2a5d376cd909bcd66
Solodovnikova Ekaterina <colo18@yandex.ru>
2017-12-16 13:01:59 +0500

    deleted line in main

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,7 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("World is not so lovely, as I am");
         System.out.println("And another one");
-        System.out.println("And me )00))0 ");
     }
 }
 
